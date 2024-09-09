a2401f3957ff39612b3e829e52122c303c8d9d2e
Naka <bacondoor1313@gmail.com>
2024-09-09 15:53:45 +0900

    modify settiong

--- carehelper/settings.py
+++ carehelper/settings.py
@@ -185,6 +185,10 @@ DEFAULT_CHARSET = 'utf-8'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
