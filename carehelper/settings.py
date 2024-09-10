1cf4856f1cdc012cd67db23178615ed7591c0a14
Naka <bacondoor1313@gmail.com>
2024-09-10 15:53:40 +0900

    change model

--- carehelper/settings.py
+++ carehelper/settings.py
@@ -206,7 +206,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'SECURE': True,
 }
 
 # Cloudinaryをデフォルトのメディアストレージに設定
