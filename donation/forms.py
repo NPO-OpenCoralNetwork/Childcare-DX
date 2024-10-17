38cca96d03ac656f8b46b08e59335d00b75724e8
Naka <bacondoor1313@gmail.com>
2024-10-17 21:47:37 +0900

    寄付フォームラベル変更

--- donation/forms.py
+++ donation/forms.py
@@ -1,18 +1,34 @@
 from django import forms
 from .models import Donation
-
 class GeneralDonationForm(forms.ModelForm):
     class Meta:
         model = Donation
-        fields = ['amount', 'is_anonymous', 'name', 'address', 'phone_number', 'reason']
+        fields = ['amount', 'is_anonymous', 'name', 'address', 'phone_number']
+        labels = {
+            'amount': '金額',
+            'is_anonymous': '匿名希望',
+            'name': '名前',
+            'address': '住所',
+            'phone_number': '電話番号',
+            'reason': '',
+        }
     
     def clean_amount(self):
         amount = self.cleaned_data['amount']
         if amount < 100:
-            raise forms.ValidationError("Amount must be at least 100 yen.")
+            raise forms.ValidationError("金額は100円以上でなければなりません。")
         return amount
 
 class MembershipDonationForm(forms.ModelForm):
     class Meta:
         model = Donation
-        fields = ['amount', 'payment_frequency', 'is_anonymous', 'name', 'address', 'phone_number', 'reason']
+        fields = ['amount', 'payment_frequency', 'is_anonymous', 'name', 'address', 'phone_number']
+        labels = {
+            'amount': '金額',
+            'payment_frequency': '',
+            'is_anonymous': '匿名希望',
+            'name': '名前',
+            'address': '住所',
+            'phone_number': '電話番号',
+            'reason': ''
+        }
