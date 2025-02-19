199be54db09e2121b29746bb34ea6e2c9fa9e0a8
Naka <bacondoor1313@gmail.com>
2025-02-19 23:14:28 +0900

    fix requirement

--- childhelper/settings.py
+++ childhelper/settings.py
@@ -153,12 +153,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', 'mydatabase'),
-        'USER': os.getenv('DB_USER', 'myuser'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'mypassword'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ikuji',
+        'USER': 'postgres',
+        'PASSWORD': 'hamberthambert1326',
+        'HOST': 'localhost',  
+        'PORT': '5432', 
     }
 }
 
@@ -204,8 +204,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
