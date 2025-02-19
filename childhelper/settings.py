7ae7e9a61a342a280580a2086e4674b02f74f082
Naka <bacondoor1313@gmail.com>
2025-02-19 23:22:59 +0900

    fix requirement

--- childhelper/settings.py
+++ childhelper/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 # CORS_ALLOW_ALL_ORIGINS = True
