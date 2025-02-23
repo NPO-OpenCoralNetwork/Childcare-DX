d363a0ef085bd914039f7bca10f6f9c3abf1117d
Naka <bacondoor1313@gmail.com>
2025-02-23 15:58:34 +0900

    change pss

--- childhelper/settings.py
+++ childhelper/settings.py
@@ -82,7 +82,7 @@ EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'zkvh oaon jqgn dbkn'
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 ROOT_URLCONF = 'childhelper.urls'
