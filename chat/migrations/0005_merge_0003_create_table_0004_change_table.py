938db3744f0519404735aa1782035e1688beb83f
Naka <bacondoor1313@gmail.com>
2025-03-01 19:08:35 +0900

    create table

--- chat/migrations/0005_merge_0003_create_table_0004_change_table.py
+++ chat/migrations/0005_merge_0003_create_table_0004_change_table.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-     
+        ('chat', '0003_create_table'),
         ('chat', '0004_change_table'),
     ]
 
