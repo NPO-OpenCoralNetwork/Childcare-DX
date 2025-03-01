47d5c8aac6b9df54ad2679f205150ec1469182dc
Naka <bacondoor1313@gmail.com>
2025-03-01 17:44:21 +0900

    requirement

--- childhelper/asgi.py
+++ childhelper/asgi.py
@@ -1,16 +1,19 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "childhelper.settings")
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
+from chat import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'childhelper.settings')
+
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns
+            routing.websocket_urlpatterns
         )
     ),
 })
