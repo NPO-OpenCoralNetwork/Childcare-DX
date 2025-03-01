388a53a8beb6d07ba7264e58f45d8425c50a5b6e
Naka <bacondoor1313@gmail.com>
2025-03-01 18:47:43 +0900

    create chat table

--- chat/migrations/0003_create_table.py
+++ chat/migrations/0003_create_table.py
@@ -2,11 +2,14 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('chat', '0002_rename_timestamp_chat_created_at_remove_chat_message_and_more'),  # 依存関係を最新のマイグレーションに更新
+        ('chat', '0002_rename_timestamp_chat_created_at_remove_chat_message_and_more'),
     ]
 
     operations = [
         migrations.RunSQL("""
+            DROP TABLE IF EXISTS channels_postgres_groupchannel;
+            DROP TABLE IF EXISTS channels_postgres_message;
+
             CREATE TABLE IF NOT EXISTS channels_postgres_message (
                 id SERIAL PRIMARY KEY,
                 channel TEXT NOT NULL,
@@ -14,15 +17,16 @@ class Migration(migrations.Migration):
                 created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
                 expires TIMESTAMP WITH TIME ZONE
             );
-            CREATE INDEX IF NOT EXISTS channels_postgres_message_channel_idx 
-            ON channels_postgres_message(channel);
-            
+
             CREATE TABLE IF NOT EXISTS channels_postgres_group (
                 group_name TEXT PRIMARY KEY,
                 created TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
             );
+
+            CREATE INDEX IF NOT EXISTS channels_postgres_message_channel_idx 
+            ON channels_postgres_message(channel);
         """, """
-            DROP TABLE IF EXISTS channels_postgres_message;
             DROP TABLE IF EXISTS channels_postgres_group;
+            DROP TABLE IF EXISTS channels_postgres_message;
         """)
     ]
