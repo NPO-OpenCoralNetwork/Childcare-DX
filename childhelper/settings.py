154c11c0331cddac3574b12d15d2946df112e2a0
Naka <bacondoor1313@gmail.com>
2025-03-01 18:09:27 +0900

    requirement

--- childhelper/settings.py
+++ childhelper/settings.py
@@ -16,11 +16,11 @@ from datetime import timedelta
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
